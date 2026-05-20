a984849da0efbc94eee90285b62ef95b1b6d262b
Aditya Chauhan <aditya.kchauhan04@gmail.com>
2026-05-20 19:09:58 +0530

    update base url

--- Frontend/src/features/interview/services/interview.api.js
+++ Frontend/src/features/interview/services/interview.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "http://localhost:3000|| https://full-stack-gen-ai-job-prep-platform-1.onrender.com",
     withCredentials: true,
 })
 
