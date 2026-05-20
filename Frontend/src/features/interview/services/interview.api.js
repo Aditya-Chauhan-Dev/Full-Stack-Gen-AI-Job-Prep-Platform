b32d89beb5a05c18ae8dbc5e61a376a9f84ea090
Aditya Chauhan <aditya.kchauhan04@gmail.com>
2026-05-20 19:22:25 +0530

    final depoly

--- Frontend/src/features/interview/services/interview.api.js
+++ Frontend/src/features/interview/services/interview.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3000|| https://full-stack-gen-ai-job-prep-platform-1.onrender.com",
+    baseURL: "https://full-stack-gen-ai-job-prep-platform-1.onrender.com", //http://localhost:3000
     withCredentials: true,
 })
 
